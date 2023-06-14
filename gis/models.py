4ab37c2a93bebfaa2384aa922d162425d8b9f26b
mgpo-98 <csi4613@naver.com>
2023-06-14 19:37:41 +0900

    0614

--- gis/models.py
+++ gis/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class farm(models.Model):
-    farm_num = models.CharField(max_length=255)
-    farm_name = models.CharField(max_length=50)
-    lat = models.CharField(max_length=255)
-    long = models.CharField(max_length=255)
-    distance = models.CharField(max_length=255)
-    result = models.CharField(max_length=255)
+    farm_num = models.CharField(max_length=150)
+    farm_name = models.CharField(max_length=150)
+    lat = models.CharField(max_length=150)
+    long = models.CharField(max_length=150)
+    distance = models.CharField(max_length=150)
+    result = models.CharField(max_length=150)
 
 
 
